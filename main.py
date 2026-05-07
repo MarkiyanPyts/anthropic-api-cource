add82a2f99c2ccda6d2c64f1508c3e9ba3ec25cc
Markiyan Pyts <markiyan.pyts@gmail.com>
2026-05-07 18:38:46 +0300

    modify

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 def greeting():
     return "Hello, welcome to the Anthropic API course!"
+
+
+def calculate_pi(num_terms: int = 1000000) -> float:
+    """Calculate pi using the Leibniz formula:
+    pi/4 = 1 - 1/3 + 1/5 - 1/7 + ...
+    """
+    pi = 0.0
+    for i in range(num_terms):
+        pi += ((-1) ** i) / (2 * i + 1)
+    return pi * 4
